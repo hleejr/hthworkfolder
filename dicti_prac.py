2bf100f971ff65137f3ad305970ece06ad77cd4f
Henry <hleejr0103@gmail.com>
2021-06-17 12:13:13 -0700

    added functions to dictionary practice

--- dicti_prac.py
+++ dicti_prac.py
@@ -1,30 +1,4 @@
 '''
-Chicken $1.59
-Beef $1.99
-Cheese $1.00
-Milk $2.50
-
-Next is updating existing values in a dictionary. To update the value for a given key we use the same bracket notation approach and variable reassignment. We can either replace the value entirely or modify it using arithmetic operators. Using the same NBA_players dictionary as an example, what if I wanted to change Lebron’s jersey number to 6, which was his number during his time in Miami? 
-
-Jordan 13,  1
-Yeezy, 8
-Foamposite, 10
-Air Max, 5
-SB Dunk, 20
-
-Update your new dictionary and its stored values based on the prompts below and print your results:
-- A customer came in to purchase 2 pairs of  SB Dunks
-- A customer came in to return a pair of Yeezys
-- The store got a new shipment. All stock increases by 7.
-- There is a special sale at the store. All stock decreases by 3.
-
-git commit commands:
-
-1. git push * 
-2. git commit -m "adding dictionary practice file"
-3. git push
-
-
 
 '''
 # The _ of _ is _ (The grocery price of chicken is 1.59)
@@ -69,10 +43,39 @@ NBA_jersey_numbers["Donovan Mitchell"] = 45
 del NBA_jersey_numbers["Kevin Durant"]
 
 chicken_price = grocery_price["Chicken"]
-print(chicken_price)
+# print(chicken_price)
 
 steph_jersey = NBA_jersey_numbers["Stephen Curry"]
-print(steph_jersey)
+# print(steph_jersey)
 
 NBA_jersey_numbers["Lebron James"] -= 17
-print(NBA_jersey_numbers)
+# print(NBA_jersey_numbers)
+print(stock_number)
+
+def total_price( item1, item2 ):
+    price1 = grocery_price[item1]
+    price2 = grocery_price[item2]
+    total = price1 + price2
+
+    return total
+
+def price_difference( item1, item2 ):
+    price1 = grocery_price[item1]
+    price2 = grocery_price[item2]
+    price_difference = abs(price1 - price2)
+
+    return price_difference
+
+def restock( shoe_name, num ):
+    old_stock = stock_number[shoe_name]
+    stock_number[shoe_name] = old_stock * num
+
+    return stock_number
+
+def clearance_sale( shoe_name, num ):
+    old_stock = stock_number[shoe_name]
+    stock_number[shoe_name] = old_stock // num
+
+    return stock_number
+
+print( clearance_sale( "SB Dunk", 5 ) )
